7b660c81e1112f7a2c1f3ab101bb919ae738b3ec
pradeep-jais <pradeepjais13@gmail.com>
2022-12-26 22:24:12 +0530

    Decimal point added, shadow and hover effect added

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ let result;
 let mathType;
 let expression;
 let flag = false;
+
 // Event listeners
+
 nums.forEach(function (num) {
   num.addEventListener('click', getValues);
 });
@@ -31,23 +33,25 @@ operations.forEach(function (operation) {
 enter.addEventListener('click', math);
 
 // Functions
+
+// Get two values from user
 function getValues(e) {
   if (!flag) {
     const num = e.currentTarget.dataset.id;
     num1 += num;
-    num1 = parseFloat(num1);
     console.log('num1', num1);
     answer.textContent = num1;
   } else {
     const num = e.currentTarget.dataset.id;
     num2 += num;
-    num2 = parseFloat(num2);
     console.log('num2', num2);
     answer.textContent = num2;
   }
 }
 // Maths
 function math(e) {
+  num1 = parseFloat(num1);
+  num2 = parseFloat(num2);
   switch (mathType) {
     case 'plus': {
       result = num1 + num2;

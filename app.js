d30f32a6ad28027efe17a36513a8ee809709b66b
pradeep-jais <pradeepjais13@gmail.com>
2022-12-26 23:26:58 +0530

    all clear, backspace function added.. bugs like answer without both value fixed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const answer = document.querySelector('.answer');
 const nums = document.querySelectorAll('.num');
 const operations = document.querySelectorAll('.operation');
 const enter = document.getElementById('result');
-const backspace = document.getElementById('backspace');
+const del = document.getElementById('backspace');
 const clear = document.getElementById('clear');
 
 let num1 = '';
@@ -23,6 +23,7 @@ nums.forEach(function (num) {
 
 operations.forEach(function (operation) {
   operation.addEventListener('click', function (e) {
+    if (num1 === '') return;
     flag = true;
     mathType = e.currentTarget.dataset.id;
     exp.textContent = `${num1}`;
@@ -31,11 +32,15 @@ operations.forEach(function (operation) {
 });
 
 enter.addEventListener('click', math);
-
+clear.addEventListener('click', clearAll);
+del.addEventListener('click', backspace);
 // Functions
 
 // Get two values from user
 function getValues(e) {
+  if (num1 === '') {
+    exp.textContent = '';
+  }
   if (!flag) {
     const num = e.currentTarget.dataset.id;
     num1 += num;
@@ -50,38 +55,66 @@ function getValues(e) {
 }
 // Maths
 function math(e) {
+  // If equal pressed without entering 2nd number
+  if (num2 === '') return;
   num1 = parseFloat(num1);
   num2 = parseFloat(num2);
   switch (mathType) {
     case 'plus': {
       result = num1 + num2;
       console.log(result);
-      displayAndResetCalc('+');
+      displayResult('+');
+      resetCalc();
       break;
     }
     case 'minus': {
       result = num1 - num2;
-      displayAndResetCalc('-');
+      displayResult('-');
+      resetCalc();
       break;
     }
     case 'mul': {
       result = num1 * num2;
-      displayAndResetCalc('*');
+      displayResult('*');
+      resetCalc();
       break;
     }
     case 'div': {
       result = num1 / num2;
-      displayAndResetCalc('/');
+      displayResult('/');
+      resetCalc();
       break;
     }
+    default:
+      break;
   }
 }
 
-// reset calculator
-function displayAndResetCalc(mathType) {
+// display result
+function displayResult(mathType) {
   exp.textContent = `${expression} ${mathType} ${num2}`;
   answer.textContent = `Ans       ${result}`;
+}
+// reset calculator
+function resetCalc() {
   flag = false;
-  num1 = 0;
-  num2 = 0;
+  num1 = '';
+  num2 = '';
+  mathType = '';
+}
+// clear display
+function clearAll() {
+  exp.textContent = '';
+  answer.textContent = '';
+  resetCalc();
+}
+// Backspace
+function backspace() {
+  if (!flag) {
+    num1 = num1.slice(0, -1);
+    answer.textContent = num1;
+  } else {
+    num2 = num2.slice(0, -1);
+    answer.textContent = num2;
+  }
 }
